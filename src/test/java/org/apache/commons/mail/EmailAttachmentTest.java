5f2bdf2459d2d667dea6f19934c7435f683d3919
Afifa Khan <khanafifa.25@gmail.com>
2020-12-02 13:56:15 -0600

    Update EmailAttachmentTest

--- src/test/java/org/apache/commons/mail/EmailAttachmentTest.java
+++ src/test/java/org/apache/commons/mail/EmailAttachmentTest.java
@@ -1,40 +1,53 @@
 package org.apache.commons.mail;
 import org.apache.commons.mail.EmailAttachment;
-
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import java.net.URL;
-
+import org.junit.Before;
+import org.junit.Test;
 import javax.mail.MessagingException;
 
-import org.junit.Test;
 
-
-
-public class EmailAttachmentTest {
+public class EmailAttachmentTest { 
+	EmailAttachment testAttachment;
 	
-EmailAttachment testAttachment;
-	
+	@Before
 	public void setUp() throws MessagingException {
 		testAttachment = new EmailAttachment();
 	}
-	
-	/*Test getName() method */
+	@Test
+	public void testgetPath() throws Exception {
+		System.out.println("lgy__");
+		System.out.println("Class Name: EmailAttachmentTest");
+		System.out.println("This is the test for setPath() method.");
+		testAttachment.setPath("//test//");	
+		assertEquals("//test//",testAttachment.getPath());
+	}
 	@Test
 	public void testgetname() throws Exception {
-		System.out.println("fwhxxx- This is a message from the EmailAttachmentTest"
-				+ "Test method for getName()");
-		testAttachment.setName("anc.jpg");	
-		assertEquals("abc.jpg", testAttachment.getName());
+		System.out.println("This is the test for setName() method.");
+		testAttachment.setName("photo.jpg");	
+		assertEquals("photo.jpg", testAttachment.getName());
 	}
-	
-	/*Test getURL() method */
 	@Test
-	public void testGetURL() throws Exception {	
-		System.out.println("fwhxxx- This is a message from the EmailAttachmentTest"
-				+ "Test method for getURL()");
-		URL myURL = new URL("http://xyz.com/");
+	public void testgetDescription() throws Exception {
+		System.out.println("This is the test for setDescription() method.");
+		testAttachment.setDescription("This is a test attachment");
+		assertEquals("This is a test attachment", testAttachment.getDescription());
+	}
+	@Test
+	public void testgeturl() throws Exception {	
+		System.out.println("This is the test for setURL() method.");
+		URL myURL = new URL("http://example.com/");
 		testAttachment.setURL(myURL);	
 		assertEquals(myURL, testAttachment.getURL());
 	}
- }
+	@Test
+	public void testgetDisposition() throws Exception {	
+		System.out.println("This is the test for setDisposition() method.");
+		testAttachment.setDisposition("Test");	
+		assertEquals("Test", testAttachment.getDisposition());
+	}
+	
+	
+	
+}
